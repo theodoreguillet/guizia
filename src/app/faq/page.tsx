f741be2818bf9ad2647d3f81f57e33139dfb6af3
Shamzamg <shamsdine.ziani@etu.univ-lyon1.fr>
2025-02-23 12:34:41 +0100

    Fill FAQ page

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -6,31 +6,36 @@ import styles from './FAQ.module.css';
 import { FAQCard } from '../components/FAQCard/FAQCard';
 
 export default function FAQ() {
+  const tokenContract = process.env.NEXT_PUBLIC_TOKEN_CONTRACT as string;
+
   return (
     <div className={styles.faqContainer}>
       <p className="font-bold text-5xl mb-10">FAQ</p>
       <p className="text-neutral-50 mb-3">
-        {`Don't find your question here ? Reach out to us on Discord or X.`}
+        {`Don't find your question here ? Reach out to us on X @GuiziaAI.`}
       </p>
       <FAQCard
         title="What is Guizia ?"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc bibendum, dui imperdiet elementum cursus, dolor massa volutpat justo, vitae tempor sapien elit ut leo. Vestibulum porttitor sapien at odio blandit, sit amet laoreet enim ornare. Nulla vestibulum, tellus ut consectetur laoreet, mauris risus interdum tortor, vel aliquam ex purus at quam. Nam urna odio, mattis vel felis eget"
+        description="Guizia is a DefAI project built on Sonic blockchain. Guizia has an autonomous behaviour seen through his twitter account and a $Guizia token that fuels the mint of beautiful raccoons. All powered by AI."
       />
       <FAQCard
         title="How to mint an NFT using Guizia ?"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc bibendum, dui imperdiet elementum cursus, dolor massa volutpat justo, vitae tempor sapien elit ut leo. Vestibulum porttitor sapien at odio blandit, sit amet laoreet enim ornare. Nulla vestibulum, tellus ut consectetur laoreet, mauris risus interdum tortor, vel aliquam ex purus at quam. Nam urna odio, mattis vel felis eget"
+        description="To mint a Guizia NFT, you need to buy credits using $GUIZIA token. Each credit costs 55555 $GUIZIA. One credit gives access to one mint. The first transaction when buying credits is to give allowance of spending 55555 tokens to the Guizia NFT contract. If you already gave allowance, there is a single transaction to buy credits. After minting, you can see your Guizia NFTs on the Collection page."
       />
       <FAQCard
         title="What wallet should I use?"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc bibendum, dui imperdiet elementum cursus, dolor massa volutpat justo, vitae tempor sapien elit ut leo. Vestibulum porttitor sapien at odio blandit, sit amet laoreet enim ornare. Nulla vestibulum, tellus ut consectetur laoreet, mauris risus interdum tortor, vel aliquam ex purus at quam. Nam urna odio, mattis vel felis eget"
+        description="Any compatible EVM wallet should work. We recommend using Rabby wallet."
       />
       <FAQCard
         title="Which tokens are supported ?"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc bibendum, dui imperdiet elementum cursus, dolor massa volutpat justo, vitae tempor sapien elit ut leo. Vestibulum porttitor sapien at odio blandit, sit amet laoreet enim ornare. Nulla vestibulum, tellus ut consectetur laoreet, mauris risus interdum tortor, vel aliquam ex purus at quam. Nam urna odio, mattis vel felis eget"
+        description={
+          'Guizia supports $GUIZIA. The contract for $GUIZIA is: ' +
+          tokenContract
+        }
       />
       <FAQCard
         title="Where can I see my NFT ?"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc bibendum, dui imperdiet elementum cursus, dolor massa volutpat justo, vitae tempor sapien elit ut leo. Vestibulum porttitor sapien at odio blandit, sit amet laoreet enim ornare. Nulla vestibulum, tellus ut consectetur laoreet, mauris risus interdum tortor, vel aliquam ex purus at quam. Nam urna odio, mattis vel felis eget"
+        description="Once minted, your Guizia NFT is visible directly after minting, and on the /collection page. You should soon be able to trade it on paintswap.io"
       />
     </div>
   );
